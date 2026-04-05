e8ddc86d10c83af5073ad788cca31002bc0b30ab
Almudena <meritas88@hotmail.com>
2026-04-05 22:07:00 +0200

    docs: reestructuración de carpetas

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+// Se importa la biblioteca Knex
+
+import knex from 'knex';
+import knexfile from "./knexfile"
+
+// Se escoge el entorno de configuración
+
+const environment = process.env.NODE_ENV || 'development';
+const config = knexfile[environment];
+
+//Se inicializa con esa configuración
+
+const db = knex(config);
+
+export default db;
