7e96f988bb7d6f0ce4369b8c68c2a7fba90def4f
Almudena <meritas88@hotmail.com>
2026-04-13 15:50:36 +0200

    refactor: Quitada la configuración por defecto de Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Puente entre (DOM) HTML y React. Inicializa React
 
+import { StrictMode } from 'react' // Detecta errores 
+import { createRoot } from 'react-dom/client' // Crea el "root" donde React se monta
+import './index.css' // Se importa porque debe aplicarse desde el inicio
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+
+
+// React arranca. Activa el modo estricto. Renderiza <App /> y el frontend nace desde App.jsx
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   </StrictMode>,
 )
