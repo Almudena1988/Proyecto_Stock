cdcabc9b6236964aca71974e9bf335b57239cf60
Almudena <meritas88@hotmail.com>
2026-04-14 08:50:46 +0200

    feat: proxy para conectar backend y frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Se activa el plugin de React, per
+  server: {
+    proxy: { // Se definen las reglas para redirigir peticiones
+      '/api': { // Prefijo que usa Vite como proxy
+        target: 'http://localhost:4000/', // Backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Elimina /api de la URL antes de enviarla al backend
+      }
+    }
+  }
 })
+
