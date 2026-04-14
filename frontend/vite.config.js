90d32e292bd4de06588aa9bd82768e448a05a673
Almudena <meritas88@hotmail.com>
2026-04-14 14:26:40 +0200

    fix: Arreglado problema que daba con la API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Se activa el plugin de React, per
+  plugins: [react()], // Se activa el plugin de React
   server: {
     proxy: { // Se definen las reglas para redirigir peticiones
       '/api': { // Prefijo que usa Vite como proxy
         target: 'http://localhost:4000/', // Backend
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Elimina /api de la URL antes de enviarla al backend
+        //rewrite: (path) => path.replace(/^\/api/, '') // Elimina /api de la URL antes de enviarla al backend
       }
     }
   }
