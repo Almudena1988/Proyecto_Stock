955f0ad151e09cf5ebfc429f4511212d969c9ec2
Almudena <meritas88@hotmail.com>
2026-04-14 14:29:25 +0200

    feat: Se añaden enlaces

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,25 +1,32 @@
 import { Icon, NavbarDivider, NavbarGroup, Navbar } from "@blueprintjs/core";
+import { Link } from "react-router-dom";
 
-import { AlignLeft } from "@blueprintjs/icons";
 
-
-export function Menu({ darkMode, setDarkMode} ) {
+export function Menu({ darkMode, setDarkMode }) {
     return (
         <div>
-            <Navbar className="navbar">
-                <NavbarGroup>
-                    <div className="textonavbar" >
-                        <p className="texto">Gestor de Stock</p>
-                        <p className="texto2">Ayuda</p>
-                    </div>
-                    <NavbarDivider />
-                    <div >
-                        <Icon className="icons" icon="home" size={20} />
-                        <Icon className="icons" icon="contrast" size={20} onClick={() => setDarkMode(!darkMode)}/>
-                        <Icon className="icons" icon="help" size={20} />
-                    </div>
-                </NavbarGroup>
-            </Navbar>
+            <div>
+                <Navbar className="navbar">
+                    <NavbarGroup>
+                        <div className="textonavbar" >
+                            <p className="texto">Gestor de Stock</p>
+                            <p className="texto2">Ayuda</p>
+                        </div>
+                        <NavbarDivider />
+                        <div >
+                            <Icon className="icons" icon="home" size={20} />
+                            <Icon className="icons" icon="contrast" size={20} onClick={() => setDarkMode(!darkMode)} />
+                            <Icon className="icons" icon="help" size={20} />
+                        </div>
+                    </NavbarGroup>
+                </Navbar>
+            </div>
+
+            <div>
+                <p> <Link to="/">Inicio de sesión</Link></p>
+                <p> <Link to="/proveedores" >Proveedores</Link></p>
+                <p> <Link to="/productos" >Productos</Link></p>
+            </div>
         </div>
     );
 }
