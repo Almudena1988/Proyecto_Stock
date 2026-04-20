785ec92de3491fed6a0c23065391094469f31d6c
Almudena <meritas88@hotmail.com>
2026-04-20 14:57:45 +0200

    feat: Añadiendo CRUD de producto desde el front

---
+++ frontend/src/pages/updateProduct.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+// El componente de React recibe los props desde fuera
+export function ModificarProducto({
+    id,
+    name,
+    description,
+    stock_current,
+    stock_minimum,
+    supplier_id
+}) 
+{
+
+    const [newname, setName] = useState(name);
+    const [new_description, setDescription] = useState(description);
+    const [new_stock_current, setStockCurrent] = useState(stock_current);
+    const [new_stock_minimum, setStockMinimum] = useState(stock_minimum);
+    const [new_supplier_id, setSupplierId] = useState(supplier_id);
+
+    const handleEdit = async () => {
+        try {
+            const response = await fetch(`/api/v1/products/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: newname,
+                    description: new_description,
+                    stock_current: new_stock_current,
+                    stock_minimum: new_stock_minimum,
+                    supplier_id: new_supplier_id
+                })
+            });
+
+            const data = await response.json().catch(() => null);
+
+            if (response.ok) {
+                console.log("Producto actualizado");
+            } else {
+                console.log("Status:", response.status);
+                console.log("Body:", data);
+            }
+
+        } catch (error) {
+            console.log("Error modificando el producto");
+        }
+    };
+
+    return (
+        <div>
+            <input value={newname} onChange={(e) => setName(e.target.value)} />
+            <input value={new_description} onChange={(e) => setDescription(e.target.value)} />
+            <input value={new_stock_current} onChange={(e) => setStockCurrent(e.target.value)} />
+            <input value={new_stock_minimum} onChange={(e) => setStockMinimum(e.target.value)} />
+            <input value={new_supplier_id} onChange={(e) => setSupplierId(e.target.value)} />
+
+            <button onClick={handleEdit}>
+                Guardar cambios
+            </button>
+        </div>
+    );
+}
