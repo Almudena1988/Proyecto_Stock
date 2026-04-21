49c685327e48c756cfc7cefe12f7723f06f8dbb6
Almudena <meritas88@hotmail.com>
2026-04-21 12:47:37 +0200

    style: formateo de código

--- frontend/src/pages/suppliers/updateSupplier.jsx
+++ frontend/src/pages/suppliers/updateSupplier.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
 export function UpdateSupplier({
-    id, 
-    name, 
+    id,
+    name,
     email,
     address,
     created_at
-}) 
-{
+}) {
     const [new_id, setNewId] = useState(id);
     const [new_name, setNewName] = useState(name);
     const [new_email, setNewEmail] = useState(email);
@@ -15,12 +14,12 @@ export function UpdateSupplier({
     const [new_created_at, setNewCreatedAt] = useState(created_at)
 
     const handleEdit = async () => {
-        try{
-            const response = await fetch(`/api/v1/suppliers/${id}`,{
+        try {
+            const response = await fetch(`/api/v1/suppliers/${id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
-                    
+
                 },
                 body: JSON.stringify({
                     id: new_id,
@@ -28,30 +27,30 @@ export function UpdateSupplier({
                     email: new_email,
                     address: new_address,
                     created_at: new_created_at
-                })                
+                })
             });
-            
-            const data = await response.json().catch(()=> null);
 
-            if (response.ok){
+            const data = await response.json().catch(() => null);
+
+            if (response.ok) {
                 console.log("Nuevo proveedor añadido")
-            }else{
+            } else {
                 console.log("Status: ", response.status)
                 console.log("Body: ", data)
             }
 
-        }catch(error){
+        } catch (error) {
             console.log("Error al añadir proveedor")
         }
     }
 
-    return(
+    return (
         <div>
-            <input value={new_id} onChange={(e)=> setNewId (e.target.value)} />
-            <input value={new_name} onChange={(e) => setNewName (e.target.value)} />
-            <input type="email" value={new_email} onChange={(e)=> setNewEmail (e.target.value)} />
-            <input value={new_address} onChange={(e) => setNewAddress (e.target.value)} />
-            <input value={new_created_at} onChange={(e)=> setNewCreatedAt(e.target.value)} />
+            <input value={new_id} onChange={(e) => setNewId(e.target.value)} />
+            <input value={new_name} onChange={(e) => setNewName(e.target.value)} />
+            <input type="email" value={new_email} onChange={(e) => setNewEmail(e.target.value)} />
+            <input value={new_address} onChange={(e) => setNewAddress(e.target.value)} />
+            <input value={new_created_at} onChange={(e) => setNewCreatedAt(e.target.value)} />
             <button onChange={handleEdit}>Guardar cambios</button>
         </div>
     )
