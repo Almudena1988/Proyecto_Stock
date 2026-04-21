21d0cdc777eb208883c2d16b36aec350a50623ad
Almudena <meritas88@hotmail.com>
2026-04-21 08:59:39 +0200

    refactor: Datos recibidos del backend se muestran como tabla

--- frontend/src/pages/suppliers.jsx
+++ frontend/src/pages/suppliers.jsx
@@ -12,24 +12,41 @@ export function Proveedores() { // Se define el componente
         fetch("/api/v1/suppliers") // Petición a la API
             .then(res => res.json())
             .then(data => setProveedores(data)) // Guarda los datos en proveedores. Provoca que el componente se vuelva a renderizar
-            .catch(err => console.error("ERROR:", err));
+            .catch(err => console.error("Error:", err));
     }, []); // [] => el useEffect solo se ejecuta una vez al montar el componente
 
     return (
         <div>
             <h2>Proveedores</h2>
-            
-            {proveedores.length > 0 ? (// Comprueba si hay proveedores en el array. Si hay muestra la lista, si no hay datos, mensaje de carga
-                proveedores.map((p) => ( // map: método para recorrer todos los elementos de un array
-                    <div key={p.id} style={{ marginBottom: "10px", padding: "10px" }}> 
-                        <strong>{p.name}</strong>
-                        <p> <strong> Id: </strong>{p.id}</p>
-                        <p> <strong> Email: </strong>{p.email}</p>
-                        <p> <strong> Dirección: </strong>{p.address}</p>
-                        <p>{p.created_at}</p>
-                    </div>
-                ))
-            ) : (<p>Cargando o no hay datos...</p>)} 
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Id proveedor</th>
+                        <th>Email</th>
+                        <th>Dirección</th>
+                        <th>Fecha de creación</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {proveedores.length > 0 ? (proveedores.map((p) => (
+                        <tr key={p.id}>
+                            <td>{p.name}</td>
+                            <td>{p.id}</td>
+                            <td>{p.email}</td>
+                            <td>{p.address}</td>
+                            <td>{p.created_at}</td>
+                        </tr>
+                    ))) : (<tr> Cargando datos </tr>)
+
+
+                    }
+
+                </tbody>
+
+            </table>
+
+
         </div>
     );
 }
