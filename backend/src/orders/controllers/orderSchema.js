606f162f8c5d9878b48bd736c305629e33510ab9
Almudena <meritas88@hotmail.com>
2026-04-27 17:16:12 +0200

    chore: Cambio de esquema Pedido

--- backend/src/orders/controllers/orderSchema.js
+++ backend/src/orders/controllers/orderSchema.js
@@ -1,9 +1,7 @@
 import * as yup from "yup";
 
 export const orderSchema = yup.object().shape({
-    supplier_id: yup.number().required("Se necesita el id del proveedor"),
-    order_date: yup.date().required("Especifica fecha de pedido"),
-    // Solo permite ciertos valores concretos como los del CHECK de la base de datos
-    status: yup.string().oneOf(["pending", "received", "cancelled"])
-        .required("Especifica el estado del pedido")
+    name: yup.number().required("Se requiere nombre del producto"),
+    quantity: yup.number().required("Especifica cantidad de pedido")  
+    
 });
