308b714ac961919dec7f0a7446b9e6c20072265c
Almudena <meritas88@hotmail.com>
2026-04-28 11:22:36 +0200

    style: Se añaden elementos CSS

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,46 +31,56 @@ export function Login() {
     };
 
     return (
-        <div className='login'>
+        
+            <div className='login'>
 
-            <h3> Inicio de sesión</h3>
+                <div>
+                    <h1> LabStock <span style={{ color: "green" }}>Control</span></h1>
+                    <p>-Sistema de gestión de inventario-</p>
+                </div>
+                <div className='welcome-container'>
+                    <h3>Bienvenido</h3>
+                    <p>Inicio de sesión</p>
+                </div>
 
-            <h3>Usuario:</h3>
-            <div className='pass-container'>
-                <input
+                <label>Usuario:</label>
+                <div className='user-container'>
+                    <input
+                        className='user-input'
+                        type="text"
+                        placeholder='Introduce el nombre de usuario'
+                        onChange={(e) => setUsername(e.target.value)} />
+                </div>
 
-                    type="text"
-                    placeholder='Introduce el nombre de usuario'
-                    onChange={(e) => setUsername(e.target.value)} />
+                <label>Contraseña:</label>
+                <div className='pass-container'>
+                    <input
+                        className='pass-input'
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder='Introduce la contraseña'
+                        onChange={(e) => setPassword(e.target.value)} />
+
+
+                    <Icon
+                        className='hide-show-pass'
+                        icon={showPassword ? "eye-off" : "eye-open"}
+                        onClick={() => setShowPassword(!showPassword)}
+                        style={{ cursor: "pointer" }} />
+                </div>
+                <div className='remember-input'>
+                    <label> <input type="checkbox" /> Recordar usuario</label>
+                </div>
+
+                <button
+                    style={{ cursor: "pointer" }}
+                    className='enter-button'
+                    onClick={handleLogin}>
+                    Entrar
+                </button>
+
+                <p>{mensaje}</p>
             </div>
-
-            <h3>Contraseña:</h3>
-            <div>
-                <input
-
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder='Introduce la contraseña'
-                    onChange={(e) => setPassword(e.target.value)} />
-
-
-                <Icon
-                    className='hide-show-pass'
-                    icon={showPassword ? "eye-off" : "eye-open"}
-                    onClick={() => setShowPassword(!showPassword)}
-                    style={{ cursor: "pointer" }} />
-            </div>
-            <br /><br />
-
-            <button
-                style={{ cursor: "pointer" }}
-                className='enter-button'
-                onClick={handleLogin}>
-                Entrar
-            </button>
-
-            <p>{mensaje}</p>
-        </div>
-
+        
 
     );
 }
