80ec445f246881c74216b1d7ef627f3a7ef905d6
Almudena <meritas88@hotmail.com>
2026-04-29 14:38:23 +0200

    refactor: Menú lateral integrado en NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 // Componente raíz de la aplicación React. Desde aquí se construye y renderiza toda la UI
 
 import './App.css'
-import { Menu } from './pages/Menu';
 import { Login } from './pages/Login';
 import { Routes, Route } from 'react-router-dom';
 import "@blueprintjs/core/lib/css/blueprint.css";
@@ -16,25 +15,22 @@ import { CrearProducto } from './pages/products/NewProduct';
 import { NavBar } from './pages/NavBar';
 
 
-
-
 function App() {
 
   const [darkMode, setDarkMode] = useState(false);
 
   return (
     <div className={darkMode ? "dark" : "light"}>
+      <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
 
       <Routes>
-        <Route element={<NavBar darkMode={darkMode} setDarkMode={setDarkMode} />}>
-          <Route path="/" element={<Login />} />         
-          <Route path="/home" element={<Menu darkMode={darkMode} setDarkMode={setDarkMode} />} />
-          <Route path="/proveedores" element={<Proveedores />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/pedido" element={<Pedido />} />
-          <Route path="/new" element={<NewOrder />} />
-          <Route path="/newproduct" element={<CrearProducto />} />
-        </Route>
+        <Route path="/" element={<Login />} />        
+        <Route path="/home" element={<Productos darkMode={darkMode} setDarkMode={setDarkMode} />} />
+        <Route path="/proveedores" element={<Proveedores />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/pedido" element={<Pedido />} />
+        <Route path="/new" element={<NewOrder />} />
+        <Route path="/newproduct" element={<CrearProducto />} />
       </Routes>
 
     </div>
--- frontend/src/pages/NavBar.jsx
+++ frontend/src/pages/NavBar.jsx
@@ -1,50 +1,53 @@
-import { Outlet } from "react-router-dom"; // Sirve para renderizar rutas hijas dentro de este layout.
+
 import { Icon, NavbarDivider, NavbarGroup, Navbar } from '@blueprintjs/core'
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { HelpIcon } from './HelpMenu.jsx';
 
+
 export function NavBar({ darkMode, setDarkMode }) {
     const [open, setOpen] = useState(false);
 
     return (
-        <div>
-            <Navbar className="navbar" darkMode={darkMode} setDarkMode={setDarkMode} />
+        <div className="main-container">
+            <Navbar className="navbar"  />
             <NavbarGroup className="navbar-group">
 
                 <div className="textonavbar" >
                     <h2> LabStock <span style={{ color: "green" }}>Control</span></h2>
                 </div>
+                <div className="routes">
+                    <Link to="/proveedores">Proveedores</Link>
+                    <Link to="/productos">Productos</Link>
+                    <Link to="/pedido">Pedidos</Link>
+                    <Link to="/new">Nuevo pedido</Link>
+                </div>
 
-                <NavbarDivider className="navbar-divider" />
-                
                 <div className="navbar-icon">
+                    <NavbarDivider className="navbar-divider" />
                     <Link to="/" className="home-icon"> <Icon icon="home" size={20} /> </Link>
 
-                    <Icon className="mode-icon" 
-                    icon="contrast" 
-                    style={{ cursor: "pointer" }} 
-                    size={20} 
-                    onClick={() => setDarkMode(!darkMode)} />
+                    <Icon className="mode-icon"
+                        icon="contrast"
+                        style={{ cursor: "pointer" }}
+                        size={20}
+                        onClick={() => setDarkMode(!darkMode)} />
 
                     {/* onClick siempre recibe una función. Esa función puede:
                     cambiar de estado, llamar funciones*/}
-                    <button onClick={() => setOpen(true)}>
-                        <Icon className="help-icon" 
-                        icon="help" 
-                        size={20} 
+
+                    <Icon className="help-icon"
+                        onClick={() => setOpen(true)}
+                        icon="help"
+                        size={20}
                         style={{ cursor: "pointer" }} />
-                    </button>
 
                     <HelpIcon open={open}
                         handleClose={() => setOpen(false)} ></HelpIcon>
                 </div>
 
             </NavbarGroup>
-
-            <div>
-                <Outlet /> {/* React Router renderiza las rutas hijas*/}
-            </div>
+            
         </div>
     );
 }
