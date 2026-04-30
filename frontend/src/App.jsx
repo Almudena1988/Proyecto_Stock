280bc75909e10029ca1d67e1ca33be83c426e530
Almudena <meritas88@hotmail.com>
2026-04-30 11:01:23 +0200

    chore: Implementación de useLocation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 // Componente raíz de la aplicación React. Desde aquí se construye y renderiza toda la UI
 
 import './App.css'
-import { Login } from './pages/Login';
-import { Routes, Route } from 'react-router-dom';
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "normalize.css";
+import { Login } from './pages/Login';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import { Proveedores } from './pages/suppliers/Suppliers'
 import { Productos } from './pages/products/Products';
@@ -13,17 +13,28 @@ import { Pedido } from './pages/order/Order'
 import { NewOrder } from './pages/order/NewOrder';
 import { CrearProducto } from './pages/products/NewProduct';
 import { NavBar } from './pages/NavBar';
+import { useLocation } from 'react-router-dom';
+
+// useLocation devuelve un objeto tipo location. Dentro de ese objeto hay información sobre la URL
+// que el usuario ha pedido. Esa información se guarda en pathname
 
 
 function App() {
 
   const [darkMode, setDarkMode] = useState(false);
+  const location = useLocation();
+  
 
   return (
     <div className={darkMode ? "dark" : "light"}>
-      <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
 
-      <Routes>
+      {location.pathname !== "/" && (
+        <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
+      )}     
+
+      <Routes> {/*Agrupa las rutas*/}
+
+        {/* Renderiza un componente en función de la ruta que definamos en el navegador */}
         <Route path="/" element={<Login />} />        
         <Route path="/home" element={<Productos darkMode={darkMode} setDarkMode={setDarkMode} />} />
         <Route path="/proveedores" element={<Proveedores />} />
