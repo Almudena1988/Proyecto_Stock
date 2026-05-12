6b30d7de2fece261ee367e004a352bae2ab4bab8
Almudena <meritas88@hotmail.com>
2026-05-12 16:18:41 +0200

    feat: Añadidas las tablas de la base de datos para Migraciones

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,16 @@
 // Se importa la biblioteca Knex y el archivo de configuración de Knex
 
-
 import knex from 'knex';
 import knexfile from "../../knexfile.js"
 
-// Se escoge el entorno de configuración
 
-const environment = process.env.NODE_ENV || 'development';
+
+const environment = process.env.NODE_ENV || 'development'; 
 const config = knexfile[environment];
 
 //Se inicializa con esa configuración
 
 const db = knex(config);
+db.migrate.latest();
 
 export default db;
--- backend/src/db/migrations/20260406104955_migracion.js
+++ backend/src/db/migrations/20260406104955_migracion.js
@@ -2,22 +2,45 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function up (knex) {
-    return knex.schema.createTable('suppliers', table => {
-        table.increments('id').primary();
+export async function up(knex) { // Migración que se ejecuta con knex migrate:latest
+    
+    await knex.schema.createTable('suppliers', table => {
+        table.increments('id').primary(); // Autoincremental y clave primaria
         table.string('name').notNullable();
         table.string('email').unique();
         table.string('address');
-        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('created_at').defaultTo(knex.fn.now()); // Fecha y hora actual
     });
-  
+
+    await knex.schema.createTable('products', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('description');
+        table.smallint('stock_current').notNullable();
+        table.smallint('stock_minimum').notNullable();
+        // No puede tener ID negativo, no es valor nulo, referencia al campo id en la tabla supplier. 
+        // Si se borra un provedor se borran sus productos
+        table.smallint('supplier_id').unsigned().notNullable().references('id').inTable('suppliers').onDelete('CASCADE');
+
+    });
+
+    await knex.schema.createTable('orders', table => {
+        table.smallint('id').primary();
+        table.smallint('product_id').unsigned().notNullable().references('id').inTable('products');
+        table.smallint('quantity').notNullable();
+    });
+   
+
+
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function down (knex) {
-    return knex.schema.dropTable('suppliers');
-  
+export async function down(knex) {
+    await knex.schema.dropTable('suppliers');
+    await knex.schema.dropTable('products');
+    await knex.schema.dropTable('orders');
+
 };
