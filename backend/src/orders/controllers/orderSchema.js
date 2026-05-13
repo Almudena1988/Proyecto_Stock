65d9b729bb14b156e41bdadd49b2d18a5e2ca9d3
Almudena <meritas88@hotmail.com>
2026-05-13 16:14:17 +0200

    fix: Corregida validacion yup

--- backend/src/orders/controllers/orderSchema.js
+++ backend/src/orders/controllers/orderSchema.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
-export const orderSchema = yup.object().shape({
-    name: yup.number().required("Se requiere nombre del producto"),
+export const orderSchema = yup.object().shape({    
+    product_id: yup.number().required("Se requiere producto"),
     quantity: yup.number().required("Especifica cantidad de pedido")  
     
 });
